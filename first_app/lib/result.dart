c276b828d78f7af97e33c5506f71c1d66e103b67
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-10-31 14:50:19 +0100

    added first app

---
+++ first_app/lib/result.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetQuiz;
+
+  Result(this.resultScore, this.resetQuiz);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty Good!';
+    } else {
+      resultText = 'Okay-ish';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetQuiz,
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
+}
