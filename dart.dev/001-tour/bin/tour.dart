c3f9709a21afea0ce38225cf7ab638cc0e6f7cbb
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-12-07 11:58:58 +0100

    learnt Types and Functions

--- dart.dev/001-tour/bin/tour.dart
+++ dart.dev/001-tour/bin/tour.dart
@@ -19,6 +19,45 @@ void main(List<String> arguments) {
   }
 
   print(">>> $lineCount");
+
+  var s = 'string interpolation';
+
+  print("> Dart has $s, which is very handy." + lineCount.toString());
+  print("""That deserves all caps. >"<
+      "${s.toUpperCase()} is very handy!""");
+  print(r"In a raw string, not even \n gets special treatment.");
+
+  var list1 = const [
+    'Car',
+    'Boat',
+    'Plane',
+  ];
+  var list2 = ['iodine', ...list1];
+  bool isMonday = false;
+  var list3 = [...list1, if (isMonday) "Monday"];
+  var list4 = [for (var i in list3) "#$i"];
+  print("list#1: $list1");
+  print("list#2: $list2");
+  print("list#3: $list3");
+  print("list#4: $list4");
+
+  Set<String> halogens = {
+    'fluorine',
+    'chlorine',
+    'bromine',
+    'iodine',
+    'astatine',
+    ...list2,
+  };
+  print(">>> $halogens");
+
+  void printElement(int element) {
+    print("> $element");
+  }
+
+  var list = [1, 2, 3];
+  // Pass printElement as a parameter.
+  list.forEach(printElement);
 }
 
 String countInit() {
