cd70acda5c70cc52132998d1335f365d21b3887a
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-12-14 13:45:43 +0100

    try "hello world"

---
+++ docs.flutter.dev/basics/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:basics/button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const CupertinoApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
+
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for
+    // the major Material Components.
+    return Scaffold(
+      appBar: const CupertinoNavigationBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        middle: Text('Marry Christmas!'),
+        trailing: IconButton(
+          icon: Icon(Icons.search),
+          tooltip: 'Search',
+          onPressed: null,
+        ),
+        // actions: [
+        //   IconButton(
+        //     icon: Icon(Icons.search),
+        //     tooltip: 'Search',
+        //     onPressed: null,
+        //   ),
+        // ],
+      ),
+      // body is the majority of the screen.
+      body: Center(
+        child: Column(
+          children: const [Text('Ho-ho-ho, world!'), MyButton()],
+        ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add gift', // used by assistive technologies
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
