9aae76ed93d339e9f01d16bfecc0d1a51abf3873
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-12-21 17:09:24 +0100

    finished Dart Type System

--- dart.dev/002-types/bin/types.dart
+++ dart.dev/002-types/bin/types.dart
@@ -1,11 +1,19 @@
 import 'package:types/types.dart' as types;
 
 void printInt(List<int> a) => print(a);
-void main(List<String> arguments) {
+
+void main() {
   print('Hello world: ${types.calculate()}!');
 
-  final list = [];
+  final list = <int>[];
   list.add(1);
-  list.add('2');
+  list.add(2);
   printInt(list);
+
+  var arguments = {'argA': 'hello', 'argB': 42}; // Map<String, Object>
+  print(">>> $arguments");
+
+  num y = 3; // A num can be double or int.
+  y = 4.0;
+  print(">>> $y");
 }
