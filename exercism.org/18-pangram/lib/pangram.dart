bcaae417839d33f278fe5a2058b88a3a80aef095
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-01-02 12:21:39 +0100

    solved Pangram

---
+++ exercism.org/18-pangram/lib/pangram.dart
@@ -0,0 +1,36 @@
+class Pangram {
+  bool isPangram(String s) {
+    List<String> alpha = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z"
+    ];
+    s.split("").forEach((element) {
+      alpha.remove(element.toLowerCase());
+    });
+    return alpha.isEmpty;
+  }
+}
