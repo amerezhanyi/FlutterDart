611497ed2a59024856d9e0b629c70ca02615229d
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-01-10 19:00:56 +0100

    Solved "Space Age"

---
+++ exercism.org/22-space-age/lib/space_age.dart
@@ -0,0 +1,16 @@
+class SpaceAge {
+  Map<String, double> planets = {
+    "Mercury": 0.2408467,
+    "Venus": 0.61519726,
+    "Earth": 1.0,
+    "Mars": 1.8808158,
+    "Jupiter": 11.862615,
+    "Saturn": 29.447498,
+    "Uranus": 84.016846,
+    "Neptune": 164.79132,
+  };
+  double age({required String planet, required int seconds}) {
+    return double.parse(
+        (seconds / 31557600 / planets[planet]!).toStringAsFixed(2));
+  }
+}
