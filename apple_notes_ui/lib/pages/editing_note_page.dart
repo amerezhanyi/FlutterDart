5d8dcc9f87d3d7237443057b044208f4d9c46235
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-03-15 09:03:46 +0100

    add FAB

---
+++ apple_notes_ui/lib/pages/editing_note_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../models/note.dart';
+
+class EditingNotePage extends StatefulWidget {
+  final Note note;
+  final bool isNewNote;
+
+  const EditingNotePage(
+      {super.key, required this.note, required this.isNewNote});
+
+  @override
+  State<EditingNotePage> createState() => _EditingNotePageState();
+}
+
+class _EditingNotePageState extends State<EditingNotePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- apple_notes_ui/lib/pages/home_page.dart
+++ apple_notes_ui/lib/pages/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../models/note.dart';
 import '../models/note_data.dart';
+import 'editing_note_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,11 +14,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void createNewNote() {
+    final id = Provider.of<NoteData>(context, listen: false).getNotes.length;
+    Note newNote = Note(
+      id: id + 1,
+      text: '',
+    );
+    goToNotePage(newNote, true);
+  }
+
+  void goToNotePage(Note note, bool isNewNote) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditingNotePage(
+          note: note,
+          isNewNote: isNewNote,
+        ),
+      ),
+    );
+  }
+
+  // delete note
+  void deleteNote(Note note) {
+    Provider.of<NoteData>(context, listen: false).remove(note);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<NoteData>(
       builder: (BuildContext context, value, Widget? child) => Scaffold(
         backgroundColor: CupertinoColors.systemGroupedBackground,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            createNewNote;
+          },
+          elevation: 0,
+          backgroundColor: Colors.grey[300],
+          child: const Icon(
+            Icons.add,
+            color: Colors.grey,
+          ),
+        ),
         body: SafeArea(
           child: Column(
             children: [
