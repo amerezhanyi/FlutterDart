87c700bccec6768a664d17baffe5684deff3877c
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-04-19 23:12:18 +0200

    added navigation

--- admin_dashboard/lib/screens/main/main_screen.dart
+++ admin_dashboard/lib/screens/main/main_screen.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'components/side_menu.dart';
+
 class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SafeArea(
+            child: Row(
+      children: [
+        const Expanded(
+          child: SideMenu(),
+        ),
+        Expanded(
+          flex: 5,
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+      ],
+    )));
   }
 }
