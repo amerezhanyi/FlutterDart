5ed2ae22708e6e45569eb9c08ae4b22f39776428
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-05-15 22:14:34 +0200

    added My Files

--- admin_dashboard/lib/screens/dashboard/dashboard_screen.dart
+++ admin_dashboard/lib/screens/dashboard/dashboard_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'components/header.dart';
+import 'components/my_files.dart';
 import 'components/storage_details.dart';
+
 import '../../constants.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -9,25 +11,22 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: SingleChildScrollView(
-        padding: const EdgeInsets.all(defaultPadding),
+        padding: EdgeInsets.all(defaultPadding),
         child: Column(
           children: [
-            const Header(),
-            const SizedBox(height: defaultPadding),
+            Header(),
+            SizedBox(height: defaultPadding),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
                   flex: 5,
-                  child: Container(
-                    height: 100,
-                    color: Colors.white,
-                  ),
+                  child: MyFiles(),
                 ),
-                const SizedBox(width: defaultPadding),
-                const Expanded(
+                SizedBox(width: defaultPadding),
+                Expanded(
                   flex: 2,
                   child: StorageDetails(),
                 ),
