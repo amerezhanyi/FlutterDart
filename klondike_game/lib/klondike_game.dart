2e69e6c6fa5839e469a099322f85471e07bcbefd
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-06-11 06:21:46 +0200

    added Card faces

--- klondike_game/lib/klondike_game.dart
+++ klondike_game/lib/klondike_game.dart
@@ -1,16 +1,19 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
+import 'components/card.dart';
 import 'components/foundation.dart';
 import 'components/pile.dart';
 import 'components/stock.dart';
 import 'components/waste.dart';
 
 class KlondikeGame extends FlameGame {
+  static const double cardGap = 175.0;
   static const double cardWidth = 1000.0;
   static const double cardHeight = 1400.0;
-  static const double cardGap = 175.0;
   static const double cardRadius = 100.0;
   static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
 
@@ -54,13 +57,26 @@ class KlondikeGame extends FlameGame {
       ..viewfinder.position = Vector2(cardWidth * 3.5 + cardGap * 4, 0)
       ..viewfinder.anchor = Anchor.topCenter;
     add(camera);
-  }
 
-  Sprite klondikeSprite(double x, double y, double width, double height) {
-    return Sprite(
-      Flame.images.fromCache('klondike-sprites.png'),
-      srcPosition: Vector2(x, y),
-      srcSize: Vector2(width, height),
-    );
+    final random = Random();
+    for (var i = 0; i < 7; i++) {
+      for (var j = 0; j < 4; j++) {
+        final card = Card(random.nextInt(13) + 1, random.nextInt(4))
+          ..position = Vector2(100 + i * 1150, 100 + j * 1500)
+          ..addToParent(world);
+        // flip the card face-up with 90% probability
+        if (random.nextDouble() < 0.9) {
+          card.flip();
+        }
+      }
+    }
   }
+}
+
+Sprite klondikeSprite(double x, double y, double width, double height) {
+  return Sprite(
+    Flame.images.fromCache('klondike-sprites.png'),
+    srcPosition: Vector2(x, y),
+    srcSize: Vector2(width, height),
+  );
 }
